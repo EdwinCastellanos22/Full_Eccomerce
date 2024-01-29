e22960e693e18e32786429ba7ea95599d3d16954
Edwin Castellanos <99506043+EdwinCastellanos22@users.noreply.github.com>
2024-01-28 19:43:22 -0600

    Delete ApiRest directory

--- ApiRest/pagos/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import crear_pago
-
-urlpatterns = [
-    path("pago/", crear_pago, name="crear pago")
-]
--- ApiRest/web/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import index, carrito
-
-urlpatterns = [
-    path('index/', index),
-    path('carrito/', carrito),
-]
